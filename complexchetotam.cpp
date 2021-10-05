0049097ea9383fb08a68135a14a5e7f5eaad2482
ut0xxia <90979663+ut0xxia@users.noreply.github.com>
2021-10-05 19:30:47 +0300

    Add files via upload

---
+++ complexchetotam.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <complex>
+using namespace std;
+
+int main()
+{
+	double x1, y1, x2, y2;
+	
+	cin >> x1 >> y1;
+	complex<double>z1(x1, y1);
+	cin.get();
+	//w(z)=z1^2
+	complex<double>z2(x2, y2);
+	z2 = z1 * z1;
+	cout << z1 << endl;
+	cout << z2;
+	return 0;
+}
